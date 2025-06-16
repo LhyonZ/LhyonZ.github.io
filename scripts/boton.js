24751cecd66d99cc26dc7f7eaafe36334b626122
JCLR <leon-online@hotmail.com>
2025-06-15 17:27:21 -0700

    CEO a lifeproject, mandyficador

--- scripts/boton.js
+++ scripts/boton.js
@@ -1,7 +1,11 @@
 (function() {
     'use-strict'
-    const bDownload = document.getElementsByClassName('buttons')[0];
+    const bSetUp = document.getElementsByClassName('buttons')[0];
+    const bDownload = document.getElementsByClassName('buttons')[1];
     const element = document.getElementsByTagName('a')[0];
+    bSetUp.addEventListener('click', function() {
+        window.location.href = bSetUp.getAttribute('data-url');
+    });
     bDownload.addEventListener('click', function() {
         window.location.href = element.getAttribute('href');
     });
